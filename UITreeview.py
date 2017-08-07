6071bea80f4d8ae32b4f4eeea50c734a6b5a4bb2
RaymondS <RaymondS@192.168.0.113>
2017-08-06 23:05:08 -0500

    fixed security..

--- UITreeview.py
+++ UITreeview.py
@@ -1,4 +1,4 @@
-
+import GlobalVariables as gv
 import tkinter as tk
 from tkinter import ttk
 from PopupMenus import WidgetPopup as wp
@@ -20,8 +20,8 @@ class TagManagerView():
         self.prevSelection = None
         style = ttk.Style()
         style.theme_use('clam')
-        style.configure('Treeview', background=gv.bckbround, 
-                fieldbackground=gv.bckbround, foreground='white')
+        style.configure('Treeview', background=gv.bckGround, 
+                fieldbackground=gv.bckGround, foreground='white')
     
     def initTreeView(self):
         self.tree = ttk.Treeview(self.frame, columns=('Tag Name'))

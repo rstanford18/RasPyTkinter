82eed6f192eb52526c772b68b2c7a9b1491f139f
lordliquid <robert.cutright@outlook.com>
2017-08-07 08:45:40 -0500

    Formatting GlobalVariables

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -1,48 +1,35 @@
 
-
-
-tagToGpioXConnect = {}
-
-
-tagElements = {}
+tagToGpioXConnect 	= {}
+tagElements 		= {}
 tagElementStructure = {
-                       'x1':0,
-                       'y1':0,
-                       'x2':0,
-                       'y2':0,
-                        'w':0,
-                        'h':0,
-                     'GPIO':0,           
-              'isGPIOBound':None,
-               'GPIOParent':None,
-              'Description':None
+                       'x1'	:0,
+                       'y1'	:0,
+                       'x2'	:0,
+                       'y2'	:0,
+                        'w'	:0,
+                        'h'	:0,
+                     'GPIO'	:0,           
+              'isGPIOBound'	:None,
+               'GPIOParent'	:None,
+              'Description'	:None
                         }
 
-camElements = {}
+camElements 		= {}
 camElementStructure = ['Camera Name', 'Url', 'Port', 
                        'Path', 'User', 'Password','Enabled']
 
 
+gpioIndex 			= 	[i for i in range(1-27)]
+gpioStateDict 		= 	{1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,
+                 			10:0,11:0,12:0,13:0,14:0,15:0,16:0,
+                 			17:0,18:0,19:0,20:0,21:0,22:0,23:0,24:0,25:0,26:0}
 
 
-gpioIndex = [i for i in range(1-27)]
+AlarmArmed 			= 	False
+CountDownToAlarm 	= 	30
+CountDownState 		= 	None
 
-gpioStateDict = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,
-                 10:0,11:0,12:0,13:0,14:0,15:0,16:0,
-                 17:0,18:0,19:0,20:0,21:0,22:0,23:0,24:0,25:0,26:0}
-
-
-AlarmArmed = False
-
-CountDownToAlarm = 30
-CountDownState = None
-
-
-
-
-
-
-piFactoryXConnect = {
+piFactoryXConnect 	= {
                    'localhost':'localhost',
                   'SecurityPi':'10.0.0.21',
                 'GarageDoorPi':'192.168.1.4',
@@ -51,35 +38,31 @@ piFactoryXConnect = {
                 }
 
 
-piFactoryXConnectOld = {}
-
-
-testData = []
-
-
+piFactoryXConnectOld 	= {}
+testData 				= []
 
 ################################################### 
 
-mySqlIpAddress = ''
-mySqlUserName = ''
-mySqlPassword = ''
-mySqlDatabaseConnection = ''
-mySqlTableName = ''
-Update_Interval = 0
+mySqlIpAddress 				= ''
+mySqlUserName 				= ''
+mySqlPassword 				= ''
+mySqlDatabaseConnection 	= ''
+mySqlTableName 				= ''
+Update_Interval 			= 0
 
 ###################################################
 
-SUPER_FONT=("Verdana",36)
-LARGE_FONT=("Verdana",12)
-NORMAL_FONT=("Verdana",10)
-SMALL_FONT=("Verdana",8)
+SUPER_FONT		=	("Verdana",36)
+LARGE_FONT		=	("Verdana",12)
+NORMAL_FONT		=	("Verdana",10)
+SMALL_FONT		=	("Verdana",8)
 
 ###################################################
-fullscreen = False
-bckGround = "#333"
-forGround = "#FFF"
-forGrdRed = "#F00"
 
+fullscreen        = False
+bckGround         = "#333"
+forGround         = "#FFF"
+forGrdRed         = "#F00"
 bckGroundDisabled = "GREY"
 
 ###################################################
@@ -88,22 +71,19 @@ debugApp = False
 
 ###################################################
 
-currentDt = ""
-counter = 0
+counter         = 0
+currentDt       = ''
+password        = 'MTIzNDU2'.encode('ascii')
 
-imgConnect = "images/connecting.jpg"
-imgOffline = "images/offline.jpg"
-
-password = 'MTIzNDU2'.encode('ascii')
-
-imgBluePrint = "images/bluePrint.gif"
-
-TagElementPath = 'bin/TagElement.omf'
-CamElementPath = 'bin/CamElement.omf'
+imgConnect      = 'images/connecting.jpg'
+imgOffline      = 'images/offline.jpg'
+imgBluePrint    = 'images/bluePrint.gif'
+TagElementPath  = 'bin/TagElement.omf'
+CamElementPath  = 'bin/CamElement.omf'
 
 ###################################################
 
-labelSettings = {'width':15,
+labelSettings 	= {'width':15,
                       'background':bckGround,
                       'foreground':forGround,
                       'anchor':'e',
@@ -113,5 +93,5 @@ textBoxSettings = {'height':1,
                         'width':20,
                         'font':SMALL_FONT}
 
-entrySettings = {'width':20,
+entrySettings 	= {'width':20,
                         'font':SMALL_FONT}
